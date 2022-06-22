828a21b48a07020ecd951d4fd2665f1a5fe999b4
Damian Rotta <dipiert@gmail.com>
2022-06-21 22:44:03 -0300

    Improving tests: Fixing typo, removing unused param and add a parameterized test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,11 @@
-from flask import Flask
-import pytest
-from flask_playground.app import app as _app
-from test_db import connection, setup_database, db_session
 import uuid
 from unittest import mock
+
+import pytest
+from flask import Flask
+
 import flask_playground.db
+from flask_playground.app import app as _app
 
 
 @pytest.fixture()
@@ -37,7 +38,7 @@ def test_app_factory(app):
     assert isinstance(app, Flask)
 
 
-def test_user_succesfully_created_when_it_doesnt_exist(client, db_session):
+def test_user_successfully_created_when_it_doesnt_exist(client, db_session):
     response = client.post("/user/new", json={
         'user': str(uuid.uuid4())[:35],
         'password': 'x',
@@ -46,7 +47,7 @@ def test_user_succesfully_created_when_it_doesnt_exist(client, db_session):
 
 
 @mock.patch.object(flask_playground.app, 'get_user_by_username')
-def test_usernames_are_unique(get_user_by_username, client, db_session):
+def test_usernames_are_unique(get_user_by_username, client):
     get_user_by_username.return_value = True
     response = client.post("/user/new", json={
         'user': 'x',
@@ -54,4 +55,12 @@ def test_usernames_are_unique(get_user_by_username, client, db_session):
     })
     assert 'already exists' in response.json.get('message')
 
-# TODO: test that user and pwd fields are mandatory in post json msg
+
+@pytest.mark.parametrize(
+    "json",
+    [{'user': 'x'}, {'password': 'x'}],
+    ids=['only_user_provided', 'only_password_provided']
+)
+def test_user_and_pwd_fields_are_mandatory(json, client):
+    response = client.post("/user/new", json=json)
+    assert 'Both user and password are required' in response.json.get('message')
